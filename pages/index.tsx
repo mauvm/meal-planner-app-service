d4b36544fff428c517e60392edb77d5ed1a69ddb
Maurits van Mastrigt <maurits@nerdieworks.nl>
2020-04-29 19:54:45 +0200

    Add list of shopping list items

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,30 @@
-function HomePage() {
-  return <div>Hello world!</div>
+import axios from 'axios'
+
+function IndexPage({ items }) {
+  return (
+    <>
+      <h1>Boodschappenlijstje</h1>
+      <ul>
+        {items.map((item) => (
+          <li>{item.title}</li>
+        ))}
+      </ul>
+    </>
+  )
 }
 
-export default HomePage
+export async function getServerSideProps() {
+  const response = await axios.get(
+    'http://localhost:3000/v1/shopping-lists/unfinished-items',
+    {
+      headers: {
+        Accept: 'application/json',
+      },
+    },
+  )
+  const items = response?.data?.data || []
+
+  return { props: { items } }
+}
+
+export default IndexPage
