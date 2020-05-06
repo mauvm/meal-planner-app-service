5659cb9fc1625f3e8154e5eb72ec346e0582a823
Maurits van Mastrigt <maurits@nerdieworks.nl>
2020-05-06 13:56:48 +0200

    Fix create item API endpoint and return new ID

--- src/pages/api/create-item.ts
+++ src/pages/api/create-item.ts
@@ -7,16 +7,21 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const data = req.body.body
 
   try {
-    await axios.post(
+    const response = await axios.post(
       `${getHost()}/v1/shopping-lists/items`,
       {
         title: data.title,
       },
       {
+        // Expect 303 See Other
         maxRedirects: 0,
+        validateStatus(status) {
+          return status === HttpStatus.SEE_OTHER
+        },
       },
     )
-    res.status(HttpStatus.OK).json({})
+    const id = response.headers['x-resource-id']
+    res.status(HttpStatus.OK).json({ id })
   } catch (err) {
     res.status(HttpStatus.BAD_REQUEST).json({ message: err.message })
   }
