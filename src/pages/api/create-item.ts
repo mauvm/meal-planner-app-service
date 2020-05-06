e2b8d6b15c3a51af92c33b216d2ebe196e60624a
Maurits van Mastrigt <maurits@nerdieworks.nl>
2020-05-06 13:43:31 +0200

    Do not expect data when creating item

--- src/pages/api/create-item.ts
+++ src/pages/api/create-item.ts
@@ -7,10 +7,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
   const data = req.body.body
 
   try {
-    const response = await axios.post(`${getHost()}/v1/shopping-lists/items`, {
-      title: data.title,
-    })
-    res.status(HttpStatus.OK).json(response.data?.data)
+    await axios.post(
+      `${getHost()}/v1/shopping-lists/items`,
+      {
+        title: data.title,
+      },
+      {
+        maxRedirects: 0,
+      },
+    )
+    res.status(HttpStatus.OK).json({})
   } catch (err) {
     res.status(HttpStatus.BAD_REQUEST).end({ message: err.message })
   }
