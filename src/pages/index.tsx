8a4c9af9bf5bb6d2598a2903af9b000bada451c6
Maurits van Mastrigt <maurits@nerdieworks.nl>
2020-05-06 15:26:51 +0200

    Fix http-equiv warning in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -9,9 +9,9 @@ function IndexPage({ items }) {
         <meta charSet="UTF-8" />
         <title>Boodschappenlijstje</title>
         <meta name="robots" content="noimageindex, nofollow, nosnippet" />
-        <meta http-equiv="cache-control" content="max-age=0" />
-        <meta http-equiv="cache-control" content="no-cache" />
-        <meta http-equiv="expires" content="0" />
+        <meta httpEquiv="cache-control" content="max-age=0" />
+        <meta httpEquiv="cache-control" content="no-cache" />
+        <meta httpEquiv="expires" content="0" />
         <meta
           name="viewport"
           content="width=device-width, initial-scale=1"
