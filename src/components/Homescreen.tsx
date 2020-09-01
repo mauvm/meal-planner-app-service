551b7969f4f0397018beb8209fa4a4d84e77289f
Maurits van Mastrigt <maurits@nerdieworks.nl>
2020-09-01 16:33:54 +0200

    Add info to Homescreen

--- src/components/Homescreen.tsx
+++ src/components/Homescreen.tsx
@@ -1,6 +1,8 @@
 import { Component } from 'react'
-import { Space, Button } from 'antd'
+import { Typography, Space, Button, Divider } from 'antd'
 import { UserOutlined } from '@ant-design/icons'
+
+const { Title, Paragraph } = Typography
 
 type Props = {}
 type State = {}
@@ -8,23 +10,36 @@ type State = {}
 export default class Homescreen extends Component<Props, State> {
   render() {
     return (
-      <Space>
-        <Button
-          type="default"
-          icon={<UserOutlined />}
-          href="/api/v1/auth/login"
-        >
-          Registreren
-        </Button>
+      <>
+        <Title level={3}>Boodschappen</Title>
 
-        <Button
-          type="primary"
-          icon={<UserOutlined />}
-          href="/api/v1/auth/login"
-        >
-          Inloggen
-        </Button>
-      </Space>
+        <Space>
+          <Button
+            type="default"
+            icon={<UserOutlined />}
+            href="/api/v1/auth/login"
+          >
+            Registreren
+          </Button>
+
+          <Button
+            type="primary"
+            icon={<UserOutlined />}
+            href="/api/v1/auth/login"
+          >
+            Inloggen
+          </Button>
+        </Space>
+
+        <Divider />
+
+        <Paragraph>
+          Made with <span style={{ color: 'red' }}>♥</span> by{' '}
+          <a href="https://github.com/users/mauvm/projects/1" target="_blank">
+            Maurits
+          </a>
+        </Paragraph>
+      </>
     )
   }
 }
